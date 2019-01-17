6e29896ed3e3fcbd55b18498c64112003a38d964
alecg555 <alejo1231998@gmail.com>
2019-01-16 22:20:57 -0500

    Fixing issues 2

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let color2 = document.querySelectorAll("color2");
 
 //functions
 const changecolor = ()=>{
-	bodi.style.background = "linear-gradient(to right,"+color1.value+","+color2.value");";
+	bodi.style.background = "linear-gradient(to right,"+color1.value+","+color2.value")";
 }
 //Adding Events
 
