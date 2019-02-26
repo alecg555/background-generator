27820333d047cd87d703441345d1089326f32530
= <alejo1231998@gmail.com>
2019-02-26 10:00:09 -0500

    Añadiendo Generador Radiano

--- script.js
+++ script.js
@@ -20,3 +20,30 @@ function changecolor (){
 color1.addEventListener("input", changecolor);
 color2.addEventListener("input", changecolor);
 
+//Background Radian
+
+
+let radian1 = document.getElementById("radian1");
+let radian2 = document.getElementById("radian2");
+let radian3 = document.getElementById("radian3");
+let tipo = document.getElementById("tipo");
+let firstpor = document.getElementById("firstpor");
+let secondpor = document.getElementById("secondpor");
+let thirdpor = document.getElementById("thirdpor");
+
+const changeradiancolor = ()=>{
+    
+	body.style.backgroundImage = "radial-gradient(circle "+tipo.value+","+radian1.value+" "+firstpor.value+","+ radian2.value+" "+secondpor.value+","+radian3.value+" "+ thirdpor.value+")";
+
+	console.log("radial-gradient(circle "+tipo.value+","+radian1.value+" "+firstpor.value+","+ radian2.value+" "+secondpor.value+","+radian3.value+" "+ thirdpor.value+")");
+}
+
+
+
+
+radian1.addEventListener("input",changeradiancolor);
+radian2.addEventListener("input",changeradiancolor);
+radian3.addEventListener("input",changeradiancolor);
+
+
+
